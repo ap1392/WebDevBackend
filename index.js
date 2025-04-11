00ebca6e3f2b8a9b43d6cf337a9d4e3255748fb4
Aditya Pathak <adityap029@gmail.com>
2025-04-11 16:03:53 -0400

    env instead of hardcoding the string

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import cors from 'cors';
 import "dotenv/config";
 import session from "express-session";
 
-const CONNECTION_STRING = "mongodb://127.0.0.1:27017/kambaz"
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
 mongoose.connect(CONNECTION_STRING);
 const app = express()
 
