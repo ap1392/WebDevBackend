dbb14a0f6fd0849bfb569ce423ea667bac9c1a29
Aditya Pathak <adityap029@gmail.com>
2025-04-16 19:49:06 -0400

    Delete course

--- Kambaz/Courses/routes.js
+++ Kambaz/Courses/routes.js
@@ -12,9 +12,9 @@ export default function CourseRoutes(app) {
     const courses = await dao.findAllCourses();
     res.send(courses);
   });
-  app.delete("/api/courses/:courseId", (req, res) => {
+  app.delete("/api/courses/:courseId", async (req, res) => {
     const { courseId } = req.params;
-    const status = dao.deleteCourse(courseId);
+    const status = await dao.deleteCourse(courseId);
     res.send(status);
   });
   app.put("/api/courses/:courseId", (req, res) => {
