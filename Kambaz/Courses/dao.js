5981eab2da53379046c1e25f79fa469c2bc45f2d
Aditya Pathak <adityap029@gmail.com>
2025-04-16 19:00:39 -0400

    Course retrieval from mongo

--- Kambaz/Courses/dao.js
+++ Kambaz/Courses/dao.js
@@ -1,8 +1,8 @@
-import Database from "../Database/index.js";
 import { v4 as uuidv4 } from "uuid";
+import model from "./model.js";
 
 export function findAllCourses() {
-  return Database.courses;
+  return model.find();
 }
 
 export function findCoursesForEnrolledUser(userId) {
