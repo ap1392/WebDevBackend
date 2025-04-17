257b4fc82c8047e14c1ca644b66f88b7ab229600
Aditya Pathak <adityap029@gmail.com>
2025-04-16 20:08:57 -0400

    Modules p1

--- Kambaz/Modules/dao.js
+++ Kambaz/Modules/dao.js
@@ -1,21 +1,6 @@
-import { v4 as uuidv4 } from 'uuid';
-import Database from "../Database/index.js";
-export function findModulesForCourse(courseId) {
-  const { modules } = Database;
-  return modules.filter((module) => module.course === courseId);
-}
-export function createModule(module) {
-    const newModule = { ...module, _id: uuidv4() };
-    Database.modules = [...Database.modules, newModule];
-    return newModule;
-}
-export function deleteModule(moduleId) {
-    const { modules } = Database;
-    Database.modules = modules.filter((module) => module._id !== moduleId);
-}
-export function updateModule(moduleId, moduleUpdates) {
-    const { modules } = Database;
-    const module = modules.find((module) => module._id === moduleId);
-    Object.assign(module, moduleUpdates);
-    return module;
-}
+import moduleModel from "./model.js";
+
+export const findModulesForCourse = (courseId) => moduleModel.find({ course: courseId });
+export const createModule = (module) => moduleModel.create(module);
+export const deleteModule = (moduleId) => moduleModel.deleteOne({ _id: moduleId });
+export const updateModule = (moduleId, module) => moduleModel.updateOne({ _id: moduleId }, { $set: module });
