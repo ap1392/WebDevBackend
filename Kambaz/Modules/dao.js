d6b638cec692382a15f670a94c35f298c103a690
Aditya Pathak <adityap029@gmail.com>
2025-04-16 20:21:35 -0400

    add module

--- Kambaz/Modules/dao.js
+++ Kambaz/Modules/dao.js
@@ -1,6 +1,10 @@
 import moduleModel from "./model.js";
+import { v4 as uuidv4 } from 'uuid';
 
 export const findModulesForCourse = (courseId) => moduleModel.find({ course: courseId });
-export const createModule = (module) => moduleModel.create(module);
+export function createModule(module) {
+    const newModule = { ...module, _id: uuidv4() };
+    return moduleModel.create(newModule);
+}
 export const deleteModule = (moduleId) => moduleModel.deleteOne({ _id: moduleId });
 export const updateModule = (moduleId, module) => moduleModel.updateOne({ _id: moduleId }, { $set: module });
